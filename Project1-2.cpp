95ee0bdb7ff59d8787a47a927a232bb24be47bc7
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-03-23 21:47:41 +0600

    Add files via upload

---
+++ Project1-2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int a = 13 / 5;
+	int b = 13 % 5;
+	int c = 13.0 / 5;
+	double d = 13 / 5;
+	double e = 13 % 5;
+	double f = 13.0 / 5;
+	double g = 13 / 5 + 2 / 5;
+	double h = 13.0 / 5 + 2.0 / 5;
+	int i = 13.0 / 5 + 2.0 / 5;
+
+	cout << a << endl;
+	cout << b << endl;
+	cout << c << endl;
+	cout << d << endl;
+	cout << e << endl;
+	cout << f << endl;
+	cout << g << endl;
+	cout << h << endl;
+	cout << i << endl;
+}
