cfe3e5a4d849266fce6d969be3db57317ce3d183
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-03-23 21:37:08 +0600

    Add files via upload
    How to find x^7

---
+++ Project1-4.cpp
@@ -0,0 +1,16 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float x, x1, x2;
+    cout << "Enter x ";
+    cin >> x;
+    x1 = x * x*x;
+    x2 = x1 * x1 * x;
+    cout << "The result of x^7 is " << x2;
+}
+
+
