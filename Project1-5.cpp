6341df355554b33306a69250e2a271a85560b15e
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-03-23 21:44:26 +0600

    Add files via upload
    How to find x^8

---
+++ Project1-5.cpp
@@ -0,0 +1,16 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float x;
+    cout << "Enter the x ";
+    cin >> x;
+    x = x * x;
+    x = x * x * x * x;
+    cout << "The result of x^8 is "<<x;
+}
+
+
