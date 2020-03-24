15925cee37c2599e71241127be7ba987e3f08970
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-03-24 15:40:03 +0600

    Add files via upload
    How to find x^13

---
+++ Project1-6.cpp
@@ -0,0 +1,12 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main() {
+	float x, x1, x2;
+	cout << "Enter the x ";
+	cin >> x;
+	x1 = x * x * x;
+	x2 = x1 * x1 * x1 * x1*x;
+	cout << "The result of x^13 is " << x2;
+}
