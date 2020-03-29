66a7b462bdea84388f2a2553f36ef6659cc6414b
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-03-29 15:40:05 +0600

    Update and rename Project1-3.cpp to Project1-003.cpp

---
+++ Project1-003.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	float x;
+	cout << "Enter x ";
+	cin >> x;
+	x = x * x;
+	x = x * x * x;
+	cout << "The result is " << x;
+	return 0;
+}
+
