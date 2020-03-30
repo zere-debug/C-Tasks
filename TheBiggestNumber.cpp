592cb04d43b77a97bdc15bc1430b51b8a71d9543
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-03-30 19:48:16 +0600

    Add files via upload
    How to find the biggest number from three numbers

---
+++ TheBiggestNumber.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+double max(double a, double b) {
+	if (a > b) { return a; }
+	else { return b; }
+}
+
+double max(double a, double b, double c) {
+	return max(max(a, b), c);
+}
+
+int main() {
+	double a, b, c;
+	cout << "Enter the first number ";
+	cin >> a;
+	cout << endl;
+	cout << "Enter the next number ";
+	cin >> b;
+	cout << endl;
+	cout << "Enter the third number ";
+	cin>> c;
+	cout << endl;
+	cout << "The biggest number is ";
+	cout << max(a, b, c);
+	return 0;
+}
