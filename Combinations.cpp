b430c2c56ecfce8342546c744cf22d440f4b6aa5
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-03-30 16:46:34 +0600

    Add files via upload

---
+++ Combinations.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int factorial(int n) {
+	int f = 1, i;
+
+	for (i = 2; i <= n; ++i) {
+		f = f * i;
+	}
+
+	return f;
+}
+
+int main() {
+	int n, k;
+	cin >> n >> k;
+	cout << factorial(n) / (factorial(k) * factorial(n - k)) << endl;
+	return 0;
+}
+
+
