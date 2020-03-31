0bb040568b307f67aaa99c49f147078af0c14cc5
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-03-31 22:49:13 +0600

    Add files via upload
    How to determine the angle (in degrees) between the clockwise on the watch dial at the time point “h hours, m minutes, s seconds” and between the clockwise at midnight.

---
+++ Project1-12.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int h, m, s, x;
+	cout << "Enter hours ";
+	cin >> h;
+	cout << "Enter minutes ";
+	cin >> m;
+	cout << "Enter seconds ";
+	cin >> s;
+	h = h % 12 * 30;
+	m = m % 60 * 30 / 60;
+	s = (s % 60) * 30 / 3600;
+	cout << "The angle is " << h + m + s;
+}
