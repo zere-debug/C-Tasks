501a3cff1378453793f59cbb49d3e79616b02daa
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-03-31 23:19:05 +0600

    Add files via upload
    The program should print out the number 1 if the first number is greater than the second, the number 2 if the second is greater than the first, or the number 0 if they are equal.

---
+++ Project2-2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+
+	cout << "Enter the first number ";
+	cin >> a;
+	cout << "Enter the second number ";
+	cin >> b;
+	cout << "The result is ";
+
+	if (a > b) { cout << 1; }
+	if (b > a) { cout << 2; }
+	if (a == b) { cout << 0; }
+
+	return 0;
+}
