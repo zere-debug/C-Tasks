be775db950589b413f9e9a7143e33e7d9ab49b94
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-03-31 23:06:49 +0600

    Add files via upload
    How to find the biggest number from two numbers

---
+++ Project2-1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+
+	cout << "Enter the first number ";
+	cin >> a;
+	cout << "Enter the second number ";
+	cin >> b;
+	cout << "The biggest number is ";
+	
+	if (a > b) { cout << a; }
+	else { cout << b; }
+	
+	return 0;
+}
