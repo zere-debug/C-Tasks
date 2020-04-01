c8ec22b06232a2fc3010e86e598aace45c84afcb
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-04-01 22:20:25 +0600

    Add files via upload
    How many from three numbers are similar

---
+++ Project2-4.cpp
@@ -0,0 +1,19 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main() {
+	double a, b, c;
+	cout << "Enter the numbers ";
+	cin >> a >> b >> c;
+
+	if (a == b && b == c) { cout << 3<<" numbers is similar"; }
+
+	else if (a == b || b == c || a == c) { cout << 2<<" number is similar"; }
+
+	else {
+		cout << 0<<" numbers is similar";
+	}
+
+	return 0;
+}
