2a1507cdf2724a0f70ccf34c3b554c79608c47fb
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-04-01 22:03:28 +0600

    Add files via upload
    Determine if a triangle exists with such sides.

---
+++ Project2-3.cpp
@@ -0,0 +1,14 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main() {
+	double a, b, c;
+
+	cin >> a >> b >> c;
+
+	if (((a + b) < c) || ((b + c) < a) || ((a + c) < b)) { cout << "NO"; }
+	else { cout << "YES"; }
+
+	return 0;
+}
