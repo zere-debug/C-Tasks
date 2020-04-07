5edf0a0389c55d9f0544d8fdd4b75f85d376e9cd
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-04-07 13:47:31 +0600

    How to find the maximum of three numbers

---
+++ Project2-05.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main() {
+	double a, b, c;
+	int max = 0;
+	cout << "Enter three numbers ";
+	cin >> a;
+	cout << " ";
+	cin >> b;
+	cout << " ";
+	cin >> c;
+
+	max=a;
+	if (b > a) { max=b; }
+	if (c > a) { max=c; }
+
+	cout <<"The maximum number is "<< max;
+}
