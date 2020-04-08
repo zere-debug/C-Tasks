410b5f2e4f1925d5987b7263d7112de28cdb0f82
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-04-08 21:28:06 +0600

    Add files via upload
    A chessboard field is defined by a pair of numbers (a, b), each from 1 to 8, the first number defines the column number, the second the line number. Two cells are given. Determine if a chess rook can fall from the first square to the second in one move. Input data: 4 integers from 1 to 8 each, the first two specify the initial cell, the second two specify the final cell. The initial and final cells do not match. The program should output YES if the rook can be transferred to the second or NO otherwise from the first cell.

---
+++ Project2-06.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b, a1, b1;
+	cout << "Enter the first position and then the second position ";
+	cin >> a;
+	cout << " ";
+	cin >> b;
+	cout << " ";
+	cin >> a1;
+	cout << " ";
+	cin >> b1;
+
+	if ((a == a1) || (b == b1)) { cout << "YES"; }
+	else { cout << "NO"; }
+
+	return 0;
+}
