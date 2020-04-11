467179a04f3b1f389218d26a40b5b2a35e1e0adf
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-04-11 22:48:04 +0600

    Add files via upload

---
+++ Project2-08.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int x, y, x1, y1;
+
+	cout << "Enter the first position's coordinates";
+	cin >> x >> y;
+	cout << "Enter the second position's coordinates";
+	cin >> x1 >> y1;
+
+	int dx = x - x1;
+	int dy = y - y1;
+
+	if (dx < 0) { dx = -dx; }
+	if (dy < 0) { dy = -dy; }
+
+	if (dx==dy) { cout << "YES"; }
+	else { cout << "NO"; }
+
+	return 0; 
+}
