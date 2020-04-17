c96222fedaf25666cef06dc44c1bd1ab7c2d4af2
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-04-17 23:40:40 +0600

    Add files via upload

---
+++ Project2-11.cpp
@@ -0,0 +1,31 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b, c;
+
+	cout << "Enter the sites of triangle ";
+	cout << endl;
+
+	cin >> a >> b >> c;
+
+	cout << "The result is ";
+
+	if ((a + b) > c) {
+
+		if ((a * a + b * b) == c * c) {
+			cout << "rectangular";
+		}
+
+		if ((a * a + b * b) > c * c) {
+			cout << "acute";
+		}
+
+		if ((a * a + b * b) < c * c) {
+			cout << "obbuse";
+		}
+
+	}
+	else { cout << "Impossible triangle"; }
+}
