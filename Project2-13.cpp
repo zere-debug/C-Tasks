ef95b01e814ad07668e9f979c539db00e266cf6d
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-04-23 23:32:19 +0600

    Add files via upload

---
+++ Project2-13.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int x, k1, k10, k60;
+
+	cin >> x;
+
+	k60 = x / 60;
+	x = x % 60;
+
+	k10 = x / 10;
+	k1 = x % 10;
+
+	if (k1 * 15 > 125) {
+		k1 = 0; k10++;
+	}
+
+	if (k1 * 15 + k10 * 125 > 440) {
+		k1 = 0; k10 = 0; k60++;
+	}
+
+	cout << k1 << " " << k10 << " " << k60;
+
+	return 0;
+}
