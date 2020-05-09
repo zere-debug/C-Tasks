e6bd8a94eb945edea8d5b68fe71c9067ac643f90
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-05-09 22:53:36 +0600

    How to find 1^2+2^2+...+n^2

---
+++ Project3-1.cpp
@@ -0,0 +1,15 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, s=0, i1;
+	cout << "Enter the n numbers ";
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		i1 = i * i;
+		s = s + i1;
+	}
+
+	cout <<"The result is "<< s;
+}
