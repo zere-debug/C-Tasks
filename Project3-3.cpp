9f9691ef6c5b0aa9bd8c5daa1c40cd0e4c59384a
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-05-11 22:14:48 +0600

    How to find 1*2+2*3+...+(n-1)*n

---
+++ Project3-3.cpp
@@ -0,0 +1,13 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, s=0;
+	cout << "Enter n numbers ";
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		s = s + (i - 1) * i;
+	}
+	cout <<"The result is "<< s;
+}
