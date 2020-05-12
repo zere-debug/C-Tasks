ef6ea2e3478eab524120662849f190431d056b15
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-05-12 23:26:35 +0600

    How to find all even numbers between two numbers

---
+++ Project3-4.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+	cout << "Enter two numbers ";
+	cin >> a >> b;
+	cout << "All even numbers in this interval are ";
+	for (int i = a; i < b; i++) { 
+			if (i % 2 == 0) { cout << i << ", "; }
+		
+	}
+	if (b % 2 == 0) { cout <<b<< ". "; }
+}
