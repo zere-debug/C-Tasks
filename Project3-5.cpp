763732732e821aad542206466164b3c2ba20c27a
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-05-13 21:23:07 +0600

    How to find factorial

---
+++ Project3-5.cpp
@@ -0,0 +1,13 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, s=1, i;
+	cout << "Enter the number ";
+	cin >> n;
+	for (int i = 1; i<=n; i++) {
+		s=s*i;
+	}
+	cout <<"The factorial of number "<<n<<" is "<< s;
+}
