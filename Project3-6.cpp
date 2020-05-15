b54cd6a8c7e459681c47aacd3f20c26cffcb9936
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-05-15 22:22:44 +0600

    How to find C=n!/(k!(n-k)!)

---
+++ Project3-6.cpp
@@ -0,0 +1,15 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main() {
+	long long n, k;
+	cin >> n >> k;
+	long long res = 1;
+
+	for (int i = 1; i <= k; i++) {
+		res *= (n - k + i);
+		res /= i; 
+	}
+	cout <<"C=n!/(k!(n-k)!) = "<< res;
+}
