9791bf677fc510ae9fa7ae9817420ed0e9b77deb
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-05-16 21:05:24 +0600

    How to find a^n

---
+++ Project3-7.cpp
@@ -0,0 +1,13 @@
+﻿
+#include <iostream>
+using  namespace std;
+
+int main() {
+	int a, n, s=1;
+	cout << "Enter a and n ";
+	cin >> a >> n;
+	for (int i = 1; i <= n; i++) {
+		s = s*a;
+	}
+	cout<<a<<"^"<<n<<" = "<< s;
+}
