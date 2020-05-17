60fafd04c82917a5c7ce5db1405f34b8b108ac83
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-05-17 19:48:52 +0600

    How to find (1)+(1+2)+(1+2+3)+...+(1+2+...+n)

---
+++ Project3-8.cpp
@@ -0,0 +1,14 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, s=0, s1=0;
+	cout << "Enter the number ";
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		s = s + i;
+		s1 = s + s1;
+	}
+	cout << "The result is "<<s1;
+}
