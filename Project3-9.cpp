8b8f305ec0dabec45166fb2e31eed759c642bdf6
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-05-18 22:02:21 +0600

    How to find 1+1/2*2+1/3*3+1/4*4+...+1/n*n

---
+++ Project3-9.cpp
@@ -0,0 +1,14 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	double s = 0;
+	cout << "Enter the number ";
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		s = s + (1.0 / (i * i));
+	}
+	cout <<"The result of 1+1/2*2+1/3*3+...+1/n*n is "<< s;
+}
