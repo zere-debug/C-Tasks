7592c4cfc9410a1038008616b23ddf45feee91a2
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-05-19 21:23:01 +0600

    How to find (-1/3+1/5-1/7+...+(-1)^n/(2n+1))

---
+++ Project3-10.cpp
@@ -0,0 +1,16 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, s = -1;
+	double x = 0;
+	cout << "Enter the n ";
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		x = ((s) / (2.0 * i + 1)) + x;
+		s = -s;
+	}
+	x = x* 4;
+	cout << "The result of 4*(-1/3+1/5-1/7+...+(-1)^n/(2n+1) is "<<x;
+}
