a9634425aac06437f9639f62850d9e124b30553b
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-05-22 22:37:54 +0600

    Add files via upload

---
+++ Project3-11.cpp
@@ -0,0 +1,14 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n,a, a1=1, s=1;
+	cin >> a >> n;
+	for (int i = 0; i <= n; i++) {
+		a1= a1 * a;
+		s = s + a1;
+
+	}
+	cout << s;
+}
