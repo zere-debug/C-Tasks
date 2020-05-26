331e95b7961fb85181b43c63013f2ed2527529c4
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-05-26 22:43:53 +0600

    How to find the sum of ten different numbers

---
+++ Project3-12.cpp
@@ -0,0 +1,13 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x, result=0;
+	cout << "Enter ten different numbers ";
+	for (int i = 1; i <= 10; i++) {
+		cin >> x;
+		result += x;
+	}
+	cout <<"The sum of ten numbers are "<<result;
+}
