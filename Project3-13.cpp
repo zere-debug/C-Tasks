01060a7bea93ddfb242b6fa6a6533c40e7d44a36
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-05-31 17:27:01 +0600

    How to find a sum of three numbers

---
+++ Project3-13.cpp
@@ -0,0 +1,14 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main() {
+	int s = 0, x;
+	cout << "Enter three numbers ";
+	for (int i = 1; i <= 3; i++) {
+		cin >> x;
+		s = s + x;
+	}
+
+	cout <<"The sum of three numbers is "<< s;
+}
