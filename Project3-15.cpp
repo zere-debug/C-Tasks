bb2735ceb398b7bf85b2d5251e415e6c399cbd7d
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-06-04 22:06:15 +0600

    Add files via upload

---
+++ Project3-15.cpp
@@ -0,0 +1,15 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	for (int i = 100; i <= 999; i++) {
+		int i1, i2, i3, i4;
+		i1 = i * i;
+		i2 = i1 % 100000;
+		i3 = i2 % 10000;
+		i4 = i3 % 1000;
+		if (i4 == i) { cout << i << " "; }
+	}
+}
