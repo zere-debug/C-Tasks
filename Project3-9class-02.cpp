9fa3d7f75fe98ff2cd55bc5d65c12dedd64a7c43
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-06-07 22:33:11 +0600

    Add files via upload

---
+++ Project3-9class-02.cpp
@@ -0,0 +1,14 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a1 = 1, a, n;
+	cout << "Enter a and n ";
+	cin >> a >> n;
+
+	for (int i = 1; i <= n; i++) {
+		a1 = a1 * a;
+	}
+	cout <<"The result of a^n is "<< a1;
+}
