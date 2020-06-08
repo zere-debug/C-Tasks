4be9c2a77561168e85c0a7cc12e423cf3f8a6fd8
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-06-08 14:42:26 +0600

    Add files via upload

---
+++ Project3-9class-03.cpp
@@ -0,0 +1,15 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	double s1 = 0, s = 0, x = 1;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		s = x / (s + i);
+		s1 = s + s1;
+	}
+	cout << s1;
+}
