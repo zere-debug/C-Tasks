17c7787a117e59aae32d70c68c0deaff427647a6
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-06-09 21:48:57 +0600

    Add files via upload

---
+++ Project3-9class-04.cpp
@@ -0,0 +1,14 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, x=0;
+	cout << "Enter the number ";
+	cin >> n;
+	cout << "The dividers of this numbers are ";
+	for (int i = 1; i <= n; i++) {
+		if (n % i == 0) { x++; cout << i << " "; }
+	}
+	cout <<endl<< "The number of dividers of this number is "<<x;
+}
