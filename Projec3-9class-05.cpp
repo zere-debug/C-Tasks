c962ba6febb080cb2f1d64d6bede343bb526e3c6
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-06-10 18:52:57 +0600

    Add files via upload

---
+++ Projec3-9class-05.cpp
@@ -0,0 +1,15 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, x=0, y=0;
+	cin >> n;
+
+	for (int i = 2; i <n; i++) {
+		if (n % i == 0) { y++; }
+		else { x++; }
+	}
+	if (y++) { cout << "This number is not a prime number"; }
+	else { cout << "This number is a prime number"; }
+}
