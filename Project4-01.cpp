6e02abb34f6d8164a8b75e170c8e134049673009
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-06-14 21:25:02 +0600

    Add files via upload

---
+++ Project4-01.cpp
@@ -0,0 +1,15 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, i=1;
+	cout << "Enter the number ";
+	cin >> n;
+	cout << "The results are ";
+	while ( (i*i)<= n) {
+			cout << i*i <<" ";
+			i++;
+	   }
+	}
+
