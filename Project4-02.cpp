345be1e730c7b6a28ce268fa57c0877693a61861
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-06-16 16:57:47 +0600

    Add files via upload

---
+++ Project4-02.cpp
@@ -0,0 +1,15 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, x=0;
+	cout << "Enter the number ";
+	cin >> n;
+	while (n) {
+		x = x + (n % 2);
+		n= n / 2;
+	}
+	if (x==1) { cout <<endl<< "This number is a power of 2"; }
+	else { cout << "This number is not a power of 2"; }
+}
