9cdba2b66d7630e914d722d944163cd0bd0d441e
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-06-20 16:34:30 +0600

    Add files via upload

---
+++ Project4-03.cpp
@@ -0,0 +1,15 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, a=1, x=0;
+	cout << "Enter the number ";
+	cin >> n;
+	while (a<=n) {
+		a = a * 2;
+		x++;
+	}
+	cout <<"The lowest power of 2 is "<< x;
+	return 0;
+}
