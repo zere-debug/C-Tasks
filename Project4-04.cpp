0e017246a169e423a3aafff9a8a75ead1d3f4d5a
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-06-21 18:04:03 +0600

    Add files via upload

---
+++ Project4-04.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main() {
+	double x, y;
+	int a = 1;
+	cout << "Enter the first day's length ";
+	cin >> x;
+	cout << "Enter the last day's lenght ";
+	cin >> y;
+
+	while (x <= y) {
+		x = x * 1.1;
+		a++;
+	}
+	cout << "The sportsman will need " << a << " days to reach the lenght " << y << ".";
+}
