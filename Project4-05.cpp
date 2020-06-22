5bac147e2e68d3fcdacd1db9365369eb04fec49c
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-06-22 14:50:12 +0600

    Add files via upload

---
+++ Project4-05.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	double x, y, dist=0;
+	int days = 0;
+	cout << "Enter the first day's distance and the last day's distance ";
+	cin >> x >> y;
+	while (dist<= y) {
+		dist+=x;
+		x = x * 1.1;
+		days++;
+	}
+	cout <<"The sportsman will need "<<days<<" days to reach the distance "<<y<<" km in summary.";
+}
