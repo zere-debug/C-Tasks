5cd4331792819d199d76f70ff73525be6f06d28a
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-06-23 16:32:05 +0600

    Add files via upload

---
+++ Project4-06.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int SumOfDigits(int n) {
+	int x = 0;
+
+	while (n >= 1) {
+		x = x + n;
+		n--;
+	}
+	return x;
+}
+
+int main() {
+	int n;
+	cout << "Enter the number ";
+	cin >> n;
+	cout <<"The sum of digits of number "<<n<<" is "<<SumOfDigits(n);
+}
