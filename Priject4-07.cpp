3e6820ab855521559e3e90dd148020d5a750c991
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-06-24 16:10:02 +0600

    Add files via upload

---
+++ Priject4-07.cpp
@@ -0,0 +1,19 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int NumberOfZeroes(int n) {
+	int x=0;
+	while (n >= 9) {
+		if (n % 10 == 0) { x++; }
+		n = n / 10;
+	 }
+	return x;
+}
+
+int main() {
+	int n;
+	cout << "Enter the number ";
+	cin >> n;
+	cout <<"Number of zeroes in this number is "<< NumberOfZeroes(n);
+}
