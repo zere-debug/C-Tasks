cec385798d950118a8f82e2b0445af4bc5e83c33
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-06-26 21:50:28 +0600

    Add files via upload

---
+++ Project5-01.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int min(int a, int b) {
+	if (a < b) { return a; }
+	else { return b; }
+}
+
+int min(int a, int b, int c) {
+	return min(min(a, b), c);
+}
+
+int min(int a, int b, int c, int d) {
+	return min(min(min(a, b), c), d); 
+}
+
+int main() {
+	int a, b, c, d;
+	cout << "Enter four numbers ";
+	cin >> a >> b >> c >> d;
+	cout <<"The smallest number is "<< min(a, b, c, d);
+}
