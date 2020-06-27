a42945bab609ae2fd9267c49b52e72cf1acb013b
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-06-27 19:06:19 +0600

    Add files via upload

---
+++ Project5-02.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+using namespace std;
+
+double power(double a, int n) {
+	int s = 1;
+
+	for (int i = 1; i <= n; i++) {
+		s = s * a;
+	}
+
+	return s;
+}
+int main() {
+	int n;
+	double a;
+	cout << "Enter the number ";
+	cin >> a;
+	cout << "Enter the power ";
+	cin >> n;
+	cout << "The result is " << power(a, n);
+}
