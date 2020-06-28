7fd9bdfa6ea9c88323e693b61c504c620ab8d715
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-06-28 22:18:04 +0600

    Add files via upload

---
+++ Project5-03.cpp
@@ -0,0 +1,15 @@
+﻿
+#include <iostream>
+using namespace std;
+
+bool Xor(bool x, bool y) {
+	if ((!x && y) || (x && !y)) { return true; }
+	else { return false; }
+}
+
+int main() {
+	bool x, y;
+	cout << "Enter 1(True) or 0(False) ";
+	cin >> x >> y;
+	cout << Xor(x, y);
+}
