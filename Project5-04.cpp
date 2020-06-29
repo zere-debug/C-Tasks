09ac164fc6daf597df9813c91b96058692549312
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-06-29 22:44:12 +0600

    Add files via upload

---
+++ Project5-04.cpp
@@ -0,0 +1,17 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+bool Election(bool x, bool y, bool z) {
+	if ((x && y) || (x && z) || (y && z) || (x && z && y)) { return true; }
+	else { return false; }
+}
+
+int main() {
+	bool x, y, z;
+	cout << "Enter true(1) or false(0) ";
+	cin >> x >> y >> z;
+	cout << Election(x, y, z);
+	return 0;
+}
