ed18df1bf55da8ba9058394b3592d11caa3ace68
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-06-30 17:11:05 +0600

    Add files via upload

---
+++ Project5-05.cpp
@@ -0,0 +1,19 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+bool IsPrime(int n) {
+	for (int i = 2; i <= (n - 1); i++) {
+		if (n % i == 0) { return false; }
+	}
+	return true;
+}
+
+int main() {
+	int n;
+	cout << "Enter the number ";
+	cin >> n;
+	if (IsPrime(n)==false) { cout << "This number is composite"; }
+	else { cout << "This number is prime"; }
+}
