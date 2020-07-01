8d017e8f60c25bdeaf245c462eecd05039a7f482
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-07-01 21:22:54 +0600

    Add files via upload

---
+++ Project5-06.cpp
@@ -0,0 +1,19 @@
+﻿
+#include <iostream>
+using namespace std;
+
+double power(double a, int n) {
+	if (n == 0) { return 1; }
+	if (n == 1) { return a; } else 
+	return a * power(a,n - 1);
+}
+
+int main() {
+	int n;
+	double a;
+	cout << "Enter the number ";
+	cin >> a;
+	cout << "Enter the power ";
+	cin >> n;
+	cout <<"The result of number "<<a<<" in power "<<n<<" is "<< power(a, n)<<".";
+}
