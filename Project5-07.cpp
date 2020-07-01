b183867ad0f83adc49ae397427082744db3f9b4d
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-07-02 01:48:51 +0600

    Add files via upload

---
+++ Project5-07.cpp
@@ -0,0 +1,24 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+double power(double a, int n) {
+	if (n == 0) { return 1; }
+
+	if (n == 1) { return a; }
+
+	if (n > 1) { return a * power(a, n - 1); }
+
+	if (n < 0) { return 1.0 / a*power(1.0/a, n+1); }
+}
+
+int main() {
+	int n;
+	double a;
+	cout << "Enter the number ";
+	cin >> a;
+	cout << "Enter the power ";
+	cin >> n;
+	cout << "The result of number "<<a<<" in power of "<<n<<" is "<<power(a, n)<<".";
+}
