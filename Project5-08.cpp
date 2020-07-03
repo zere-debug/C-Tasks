3d1005b64e57c3befce83a3d871ab26ad2803555
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-07-03 22:42:49 +0600

    Add files via upload

---
+++ Project5-08.cpp
@@ -0,0 +1,22 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int power(int x, int n) {
+	if (n == 0) { return 1; }
+	if (n%2 == 0) { return  power(x, n/2)*power(x, n/2); }
+
+	if (n%2 != 0) { return x*power(x,n - 1); }
+
+	return power(x*x, n/2);
+}
+
+int main() {
+	int x, n;
+	cout << "Enter the number ";
+	cin >> x;
+	cout << "Enter the power ";
+	cin >> n;
+	cout <<"The result is "<< power(x, n)<<".";
+}
