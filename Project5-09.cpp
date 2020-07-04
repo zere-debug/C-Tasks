639fe4d5174b2881052682c9b9ea3a58fde3a644
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-07-04 16:41:26 +0600

    Add files via upload

---
+++ Project5-09.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int phi(int n) {
+	if (n == 0) { return 0; }
+
+	if (n == 1 || n == 2) { return 1; }
+
+	if (n > 1) { return (phi(n - 1) + phi(n - 2)); }
+}
+
+int main() {
+	int n;
+	cout << "Enter the n-number of Fibonachi sequence ";
+	cin >> n;
+	cout << "The n-th number of Fibonachi sequence is "<<phi(n);
+}
