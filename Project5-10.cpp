2a1b3a6482db00dbdb8a2b86ee6b02a90d37f576
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-07-05 18:34:45 +0600

    Add files via upload

---
+++ Project5-10.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int result(int n, int k) {
+	if (n < 0 || k < 0) { cout << "Cannot be count"; }
+	if (k == 0 || k == n) { return 1; }
+
+	return result(n - 1, k - 1) + result(n - 1, k);
+}
+
+int main() {
+	int n, k;
+	cout << "Enter n and k ";
+	cin >> n >> k;
+	cout <<"The number of combination from "<<n<<" by "<<k<<" is "<< result(n,k)<<".";
+}
