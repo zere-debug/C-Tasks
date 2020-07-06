537edf6dee7ba9d0c5081091419408ddb7c58a7d
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-07-06 21:24:43 +0600

    Add files via upload

---
+++ Project5-11.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <iostream>
+using namespace std;
+
+void move(int num, char from, char to, char buff) {
+
+	if (num != 0) {
+		move(num - 1, from, buff, to);
+		cout << from << "->" << to << endl;
+		move(num - 1, buff, to, from);
+	}
+}
+
+int main() {
+	int num;
+	cin >> num;
+	move(num, '1', '2', '3');
+}
