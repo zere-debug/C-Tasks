8d84404e7f4598293d5ce1e7095ee6a8c83adbd4
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-07-07 21:01:23 +0600

    Add files via upload

---
+++ Project6-01.cpp
@@ -0,0 +1,19 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+bool IsUpper(unsigned char ch) {
+	if (ch>= 'A' && ch<='Z') {
+		return true;
+	}
+	else return false;
+}
+
+int main() {
+	unsigned char ch;
+	cout << "Enter the letter ";
+	cin >> ch;
+	if (IsUpper(ch)) { cout << "Yes, it is a capital letter."; }
+	else { cout << "No, it is not a capital letter."; }
+}
