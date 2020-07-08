2f2af120feb24e8169456c7c8d265a1dd2bf8404
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-07-08 22:52:37 +0600

    Add files via upload

---
+++ Project6-02.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+using namespace std;
+
+bool IsDigit(unsigned char s) {
+
+	if ((s >= '0') && (s <= '9')) {
+		return true;
+	}
+	else return false;
+}
+
+int main() {
+	unsigned char s;
+	cout << "Enter the symbol";
+	cin >> s;
+
+	if (IsDigit(s)) {
+		cout << "Yes, it is a digit";
+	}
+	else 
+	{
+		cout << "No, it is not a digit";
+	}
+
+	return 0;
+}
