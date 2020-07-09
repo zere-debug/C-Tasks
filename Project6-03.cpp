23f2950bb3e0b7f0a4ef7f582e39cdb115113303
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-07-09 21:01:07 +0600

    Add files via upload

---
+++ Project6-03.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+using namespace std;
+
+char ToUpper(char ch) {
+	return (char(ch - 32));
+}
+
+int main() {
+	char ch;
+	cout << "Enter a small letter ";
+	cin >> ch;
+
+	if (ch >= 'a' && ch <= 'z') {
+		cout << "The uppercase letter is "<<ToUpper(ch)<<".";
+	}
+	else { cout << "Error, cannot be transform into an uppercase letter."; }
+
+	return 0;
+}
