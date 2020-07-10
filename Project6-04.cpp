608845ffde8127fc27236d2f45523f621f01f6ad
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-07-10 22:34:48 +0600

    Add files via upload

---
+++ Project6-04.cpp
@@ -0,0 +1,15 @@
+﻿
+#include <iostream>
+using namespace std;
+
+char CaesarCrypt(char ch) {
+	if (ch >= 'A' && ch <= 'Z') {
+		return ch += 3;
+	}
+}
+
+int main() {
+	char ch;
+	cin >> ch;
+	cout << CaesarCrypt(ch); 
+}
